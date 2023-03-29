2a7c558249c5826ba012998ead62b4f7d0ad1174
Marco Santini <marcosantinia12@gmail.com>
2023-03-29 11:20:10 -0700

    Addd books into my Library array and siplayed as book Cards, based off user input

--- script.js
+++ script.js
@@ -1,62 +1,74 @@
 const addABook = document.getElementById('addABook');
 const modal = document.getElementById('modal');
-// const inputForm = document.getElementById('inputForm');
+const booksGrid = document.getElementById('booksGrid');
 const bookTitleInput = document.getElementById('bookTitleInput');
 const bookAuthorInput = document.getElementById('bookAuthorInput');
 const bookPagesInput = document.getElementById('bookPagesInput');
-const bookReadInput = document.getElementById('bookReadInput');
+const readCheckbox = document.getElementById('readCheckbox');
 
-const addNewBook = document.getElementById('submitBtn');
-const booksGrid = document.getElementById('booksGrid');
-const titleOutput = 'title PH';
-const authorOutput = 'author PH';
-const pagesOutput = 'pages PH';
-const readOutput = 'Not yet read PH';
+const submitBtn = document.getElementById('submitBtn');
 
 const myLibrary = [];
 
-function Book(title, author, pages) {
-  this.tile = title;
+function Book(title, author, pages, read) {
+  this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
-// Examples of books
-const bookOne = new Book('Sapiens: A Brief History of Humankind', 'Yuval Noah Harari', 581);
-const bookTwo = new Book('Atomic Habits', 'James Clear', 340);
-const bookThree = new Book('How to Win Friends & Influence People', 'Dale Carnegie', 222);
-
 function addBookToLibrary() {
-  // do stuff here
+  const newBook = new Book(bookTitleInput.value, bookAuthorInput.value, bookPagesInput.value, readCheckbox.checked)
+  myLibrary.push(newBook);
+
+  //Clear form inputs
+  bookTitleInput.value = '';
+  bookAuthorInput.value = '';
+  bookPagesInput.value = '';
+  readCheckbox.checked = false;
+}
+
+function displayLibrary(){
+  for (let i = 0; i < myLibrary.length; i++) {
+    //console.log(myLibrary[i]);
+    const newBookCard = document.createElement('div');
+    newBookCard.className = 'bookCard';
+
+    const title = document.createElement('p');
+    title.innerHTML = myLibrary[i].title;
+    const author = document.createElement('p');
+    author.innerHTML = myLibrary[i].author;
+    const pages = document.createElement('p');
+    pages.innerHTML = myLibrary[i].pages;
+    const readBtn = document.createElement('button');
+    if(myLibrary[i].read == true){
+      readBtn.innerHTML = "Have read"
+    }else{
+      readBtn.innerHTML = "Have not read"
+        }
+    readBtn.id = "readBtn";
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'delete';
+    deleteBtn.id = 'deleteBtn';
+
+    newBookCard.appendChild(title);
+    newBookCard.appendChild(author);
+    newBookCard.appendChild(pages);
+    newBookCard.appendChild(readBtn);
+    newBookCard.appendChild(deleteBtn);
+    booksGrid.appendChild(newBookCard);
+  }
 }
 
 addABook.addEventListener('click', () => {
   modal.style.display = "flex";
 });
 
-addNewBook.addEventListener('click', () => {
-  // modal.style.display = "none";
-  const newBookCard = document.createElement('div');
-  newBookCard.className = 'bookCard';
-
-  const title = document.createElement('p');
-  title.innerHTML = titleOutput;
-  const author = document.createElement('p');
-  author.innerHTML = authorOutput;
-  const pages = document.createElement('p');
-  pages.innerHTML = pagesOutput;
-  const readBtn = document.createElement('button');
-  readBtn.innerHTML = readOutput;
-  readBtn.id = "readBtn";
-  const deleteBtn = document.createElement('button');
-  deleteBtn.innerHTML = 'delete';
-  deleteBtn.id = 'deleteBtn';
-
-  newBookCard.appendChild(title);
-  newBookCard.appendChild(author);
-  newBookCard.appendChild(pages);
-  newBookCard.appendChild(readBtn);
-  newBookCard.appendChild(deleteBtn);
-
-  booksGrid.appendChild(newBookCard);
+submitBtn.addEventListener('click', (event) => {
+  addBookToLibrary();
+  event.preventDefault();
+  modal.style.display = "none";
+  // Clear booksGrid before displaying the library
+  booksGrid.innerHTML = '';
+  displayLibrary();
 });
