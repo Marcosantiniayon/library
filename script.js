f2366607e6953f3166530171c69d5d8392c9627c
Marco Santini <marcosantinia12@gmail.com>
2023-04-12 09:22:31 -0700

    Completed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bookTitleInput = document.getElementById('bookTitleInput');
 const bookAuthorInput = document.getElementById('bookAuthorInput');
 const bookPagesInput = document.getElementById('bookPagesInput');
 const readCheckbox = document.getElementById('readCheckbox');
-
+const modalOverlay = document.querySelector('.modal-overlay');
 const submitBtn = document.getElementById('submitBtn');
 
 const myLibrary = [];
@@ -17,6 +17,15 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+closeModal();
+
+//brings up book info form 
+addABook.addEventListener('click', () => {
+  modal.style.display = "flex";
+  openModal();
+});
+
+//add book to library array with user input data
 function addBookToLibrary() {
   const newBook = new Book(bookTitleInput.value, bookAuthorInput.value, bookPagesInput.value, readCheckbox.checked)
   myLibrary.push(newBook);
@@ -28,12 +37,21 @@ function addBookToLibrary() {
   readCheckbox.checked = false;
 }
 
+//triggers addBookToLibrary Function, hides form modal, refreshes library display
+submitBtn.addEventListener('click', (event) => {
+  addBookToLibrary();
+  event.preventDefault();
+  modal.style.display = "none";
+  closeModal();
+  updateDisplay();
+});
+
+//creates book card for each book in library array & displays
 function displayLibrary(){
   for (let i = 0; i < myLibrary.length; i++) {
-    //console.log(myLibrary[i]);
+    //create book card elements
     const newBookCard = document.createElement('div');
     newBookCard.className = 'bookCard';
-
     const title = document.createElement('p');
     title.innerHTML = myLibrary[i].title;
     const author = document.createElement('p');
@@ -46,10 +64,39 @@ function displayLibrary(){
     }else{
       readBtn.innerHTML = "Have not read"
         }
+    if(myLibrary[i].read == true){
+      readBtn.classList.add('read');
+    }else{
+      readBtn.classList.add('notRead');
+        }
     readBtn.id = "readBtn";
     const deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = 'delete';
     deleteBtn.id = 'deleteBtn';
+    deleteBtn.setAttribute('data-id', i);
+    readBtn.setAttribute('data-id', i);
+
+    //Event listener for delete button
+    deleteBtn.addEventListener('click', function() {
+      const dataId = this.getAttribute('data-id');
+      myLibrary.splice(dataId, 1);
+      updateDisplay();
+    });
+
+    //Event listener for read button - update 'read' status
+    readBtn.addEventListener('click', function() {
+      const dataId = this.getAttribute('data-id');
+    if (myLibrary[dataId].read == true) {
+      myLibrary[dataId].read = false;
+      readBtn.classList.remove('read');
+      readBtn.classList.add('notRead');
+    } else {
+      myLibrary[dataId].read = true;
+      readBtn.classList.remove('notRead');
+      readBtn.classList.add('read');
+    }
+  updateDisplay();
+    });
 
     newBookCard.appendChild(title);
     newBookCard.appendChild(author);
@@ -60,15 +107,23 @@ function displayLibrary(){
   }
 }
 
-addABook.addEventListener('click', () => {
-  modal.style.display = "flex";
+//triggers addBookToLibrary Function, hides form modal, refreshes library display
+submitBtn.addEventListener('click', (event) => {
+  updateDisplay();
 });
 
-submitBtn.addEventListener('click', (event) => {
-  addBookToLibrary();
-  event.preventDefault();
-  modal.style.display = "none";
-  // Clear booksGrid before displaying the library
+function updateDisplay(){
+  // Clear booksGrid before displaying the updated library
   booksGrid.innerHTML = '';
   displayLibrary();
-});
+}
+
+function openModal() {
+  modalOverlay.classList.add('modal-overlay');
+  // ...other code to show the modal
+}
+
+function closeModal() {
+  modalOverlay.classList.remove('modal-overlay');
+  // ...other code to hide the modal
+}
